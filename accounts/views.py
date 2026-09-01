720b02743afa28c179aba50a743cc12527860211
TheBaliloo <a.kebbal@gmail.com>
2026-09-01 15:45:26 +0100

    Forth commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .forms import UserProfileForm
@@ -14,18 +14,16 @@ def home(request):
 def edit(request, user_id):
     if request.method == "GET":
         profile, created = UserProfile.objects.get_or_create(user_id = user_id)
-        form = UserProfileForm(request.GET, request.FILES, instance=profile)
+        form = UserProfileForm(instance=profile)
         context = {'form': form}
         return render(request, 'edit.html', context)
         
     if request.method == "POST":
-            profile, created = UserProfile.objects.get_or_create(user_id = user_id)
-            form = UserProfileForm(request.POST, request.FILES, instance=profile)
-            if form.is_valid():
-                form.save()
+        profile, created = UserProfile.objects.get_or_create(user_id = user_id)
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
             context = {'form': form}
             return render(request, 'edit.html', context)
-
-    context = {'form': UserProfileForm}
-    return render(request, 'edit.html', context)
-
